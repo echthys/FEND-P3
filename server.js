a476c0cfdfa68b8abe86c8cac1fc620149d452d3
Ethan <81968564+echthys@users.noreply.github.com>
2021-07-21 20:38:52 +0100

    feat: added get and post.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
-// Require Express to run server and routes
+// Required Modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +12,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
-
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
+
+const port = 8000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`Running server on port: ${port}`);
+};
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+//Get Requests
+
+app.get("/retrieveData", (req, res) => {
+    console.log("Getting project data");
+    res.send(projectData);
+})
+
+//Post Request 
+
+app.post('/appendData', (req, res) => {
+    console.log(`Body: ${req.body}`);
+    projectData = req.body;
+    res.send(projectData);
+});
+
 
